e3785876285546142bc2d1b76e1ff44faa76dc29
rafaelgiacomo <rafael.giacomo@vidibr.com>
2024-09-10 21:45:15 -0300

    criando estrutura inicial

---
+++ src/web/Agenda.WebUI/Models/EventoModel.cs
@@ -0,0 +1,24 @@
+﻿using Agenda.Common.Shared.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace Agenda.WebUI.Models;
+
+public class EventoModel
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Nome é obrigatório")]
+    [StringLength(100, ErrorMessage = "Máximo de 100 caracteres")]
+    public string Nome { get; set; }  = string.Empty;
+
+    [Required(ErrorMessage = "Telefone é obrigatório")]
+    [StringLength(100, ErrorMessage = "Máximo de 100 caracteres")]
+    public string Telefone { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "DDD é obrigatório")]
+    public DDDs? DDD { get; set; } = DDDs.SP;
+
+    [Required(ErrorMessage = "E-mail é obrigatório")]
+    [EmailAddress(ErrorMessage = "Endereço de e-mail inválido")]
+    public string Email { get; set; } = string.Empty;
+}
---
+++ src/web/Agenda.WebUI/Services/EventosService.cs
@@ -0,0 +1,36 @@
+﻿using Agenda.WebUI.Models;
+using System.Net.Http.Json;
+
+namespace Agenda.WebUI.Services
+{
+    public class EventosService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public EventosService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<List<ContatoModel>> GetEventosAsync(int? Ddd)
+        {
+            var client = _httpClientFactory.CreateClient(HttpClientNames.MyApiContatos);
+
+            var response = await client.GetAsync($"api/eventos/{Ddd}");
+            var contentType = response.Content.Headers.ContentType;
+
+            if (response.IsSuccessStatusCode
+                && contentType != null
+                && contentType.MediaType == "application/json")
+            {
+                var jsonString = await response.Content.ReadAsStringAsync();
+
+                return await response.Content.ReadFromJsonAsync<List<ContatoModel>>() ?? [];
+            }
+            else
+                return [];
+        }
+    }
+
+
+}
